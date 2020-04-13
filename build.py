dd2764992259b5888b28bc7932e63584e7d6d048
Peter Salnikov <peter@salnikov.cc>
2020-04-13 15:45:18 +0300

    Gitlab pipelines helper libs

---
+++ build.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python
+
+import re
+import os
+import sys
+from navio.builder import task, nsh, sh
+
+
+@task()
+def apidoc():
+    """
+    Generate API documentation using epydoc.
+    """
+    nsh.epydoc('--config', 'epydoc.config')
+
+
+@task()
+def validate():
+    nsh.pycodestyle('build.py', 'setup.py', '--max-line-length=180')
+    nsh.pycodestyle('navio/', '--max-line-length=180')
+
+
+@task(validate)
+def build():
+    nsh.python('setup.py', 'sdist', 'bdist_wheel', '--universal')
+
+
+@task(build)
+def test(*args):
+    """
+    Run unit tests.
+    """
+    nsh.python('setup.py', 'test')
+
+
+@task()
+def check_uncommited():
+    result = sh.git('status', '--porcelain', '--untracked-files=no')
+    if result:
+        raise Exception('There are uncommited files')
+
+
+@task()
+def update_version(ver=None):
+    with open('navio/meta_gitlab.py', 'r') as f:
+        file_str = f.read()
+
+    if not ver:
+        regexp = re.compile(r'__version__\s*\=\s*\"([\d\w\.\-\_]+)\"\s*')
+        m = regexp.search(file_str)
+        if m:
+            ver = m.group(1)
+
+    minor_ver = int(ver[ver.rfind('.') + 1:])
+    ver = '{}.{}'.format(ver[:ver.rfind('.')], minor_ver + 1)
+
+    file_str = re.sub(
+        r'__version__\s*\=\s*\"([\d\w\.\-\_]+)\"\s*',
+        r'__version__ = "{}"\n'.format(ver),
+        file_str)
+
+    with open('navio/meta_gitlab.py', 'w') as f:
+        f.write(file_str)
+
+    nsh.git('commit', 'navio/meta_gitlab.py', '-m', 'Version updated to {}'.format(ver))
+
+
+@task()
+def create_tag():
+    with open('navio/meta_gitlab.py', 'r') as f:
+        file_str = f.read()
+    regexp = re.compile(r'__version__\s*\=\s*\"([\d\w\.\-\_]+)\"\s*')
+    m = regexp.search(file_str)
+    if m:
+        ver = m.group(1)
+    else:
+        raise "Can't find/parse current version in './navio/meta_gitlab.py'"
+
+    nsh.git('tag', '-a', '-m', 'Tagging version {}'.format(ver), ver)
+
+
+@task()
+def push():
+    nsh.git('push', '--verbose')
+    nsh.git('push', '--tags', '--verbose')
+
+
+@task()
+def release(ver=None):
+    check_uncommited()
+    update_version(ver)
+    create_tag()
+    push()
+
+
+@task(test)
+def pypi():
+    args = ['upload']
+
+    travis_pull_request = os.environ.get('TRAVIS_PULL_REQUEST', False) == 'true'
+    travis_tag = os.environ.get('TRAVIS_TAG', False)
+
+    if not travis_pull_request and travis_tag:
+        args.append('--repository-url')
+        args.append('https://upload.pypi.org/legacy/')
+    else:
+        args.append('--skip-existing')
+        args.append('--repository-url')
+        args.append('https://test.pypi.org/legacy/')
+
+    args.append('dist/navio-gitlab-*')
+    nsh.twine(args)
+
+
+__DEFAULT__ = test
---
+++ navio/bitbucket/tests/test_bitbucket.py
@@ -0,0 +1,87 @@
+import pytest
+import os
+import sys
+
+
+class TestImport:
+
+    def test_import(self):
+        import navio.gitlab
+        from navio.gitlab import Gitlab
+
+
+class Test:
+
+    def test_is_gitlab(self):
+        from navio.gitlab import Gitlab
+
+        os.environ.pop('CI', None)
+        assert not Gitlab().is_gitlab()
+
+        os.environ['CI'] = 'true'
+        assert Gitlab().is_gitlab()
+
+        os.environ['CI'] = 'false'
+        assert not Gitlab().is_gitlab()
+
+        os.environ['CI'] = '???'
+        assert not Gitlab().is_gitlab()
+
+    def test_is_pull_request(self):
+        from navio.gitlab import Gitlab
+
+        os.environ.pop('CI_MERGE_REQUEST_ID', None)
+        assert not Gitlab().is_pull_request()
+
+        os.environ['CI'] = 'true'
+
+        os.environ['CI_MERGE_REQUEST_ID'] = '123'
+        assert Gitlab().is_pull_request()
+
+        os.environ['CI_MERGE_REQUEST_ID'] = '456'
+        assert Gitlab().is_pull_request()
+
+        os.environ['CI_MERGE_REQUEST_ID'] = '???'
+        assert Gitlab().is_pull_request()
+
+    def test_branch(self):
+        from navio.gitlab import Gitlab
+
+        os.environ['CI'] = 'true'
+        os.environ.pop('CI_COMMIT_BRANCH', None)
+
+        assert Gitlab().branch() is None
+
+        os.environ['CI_COMMIT_BRANCH'] = 'master'
+        assert 'master' == Gitlab().branch()
+
+        os.environ['CI_COMMIT_BRANCH'] = 'prod'
+        assert 'prod' == Gitlab().branch()
+
+    def test_commit_hash(self):
+        from navio.gitlab import Gitlab
+
+        os.environ['CI'] = 'true'
+        os.environ.pop('CI_COMMIT_SHA', None)
+
+        assert '000000000000000000000000000000' == Gitlab().commit_hash()
+
+        os.environ['CI_COMMIT_SHA'] = '1f510ab451bb4'
+        assert '1f510ab451bb4' == Gitlab().commit_hash()
+
+        os.environ['CI_COMMIT_SHA'] = '04124124bcb131'
+        assert '04124124bcb131' == Gitlab().commit_hash()
+
+    def test_short_commit_hash(self):
+        from navio.gitlab import Gitlab
+
+        os.environ['CI'] = 'true'
+        os.environ.pop('CI_COMMIT_SHA', None)
+
+        assert '0000000' == Gitlab().short_commit_hash()
+
+        os.environ['CI_COMMIT_SHA'] = '1f510ab451bb4'
+        assert '1f510ab' == Gitlab().short_commit_hash()
+
+        os.environ['CI_COMMIT_SHA'] = '04124124bcb131'
+        assert '0412412' == Gitlab().short_commit_hash()
